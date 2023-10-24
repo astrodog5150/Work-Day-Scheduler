d17df1873dc3bc8a5205df9efcab3fdb604a29a4
astrodog5150 <134658598+astrodog5150@users.noreply.github.com>
2023-10-23 23:30:54 -0500

    Updated script.js

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,51 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+    //added the time to top of page
+    var date = dayjs().toDate()
+    
+    var t = document.getElementById('current-date')
+    t.textContent = date
+    var currentTime = dayjs().hour()
+    var calendarContainer = document.getElementById('calendar-container')
+
+    var hours = [{
+        time:"9"        
+    },
+    {
+        time:"10"        
+    },
+    {
+        time:"11"        
+    },
+    {
+        time:"12"        
+    },
+    {
+        time:"13"        
+    },
+    {
+        time:"14"        
+    },
+    {
+        time:"15"        
+    },
+    {
+        time:"16"        
+    },
+    {
+        time:"17"        
+    },
+]
+//print the time slots to the page based on time
+    for (let i = 0; i < hours.length; ++i) {
+        if (time === currentTime) {
+            var currentTimeSlot = document.createElement('div');
+
+            currentTimeSlot.textContent = time;
+            calendarContainer.appendChild(currentTimeSlot);
+        }
+    }
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
